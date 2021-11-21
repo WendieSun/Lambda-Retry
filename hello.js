dd8d076f31c3b43fb8d7431e57f2b8c5c4e91a95
SunnQinnWenn <qinwensun@outlook.com>
2021-11-21 11:52:32 +1100

    add lambda function + serverless.yml

---
+++ hello.js
@@ -0,0 +1,10 @@
+
+exports.handler = function (event) {
+    if (event["error"]) {
+        throw new Error("error");
+    }
+    if (Math.random() > 0.5) {
+        throw new Error("error");
+    };
+    return;
+}
